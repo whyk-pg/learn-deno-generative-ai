0d4415c8880c5eefcd65a51aba12f37f419b52dc
WhyK <windchimeyk@gmail.com>
2024-05-02 20:11:59 +0900

    refactor: add prompt and impreved readability

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 import { ChatAnthropic } from "@langchain/anthropic";
 import "@std/dotenv/load";
 
 const chatModel = new ChatAnthropic({
   apiKey: Deno.env.get("ANTHROPIC_API_KEY"),
 });
+const outputParser = new StringOutputParser();
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a world class technical documentation writer."],
+  ["user", "{input}"],
+]);
+const chain = prompt.pipe(chatModel).pipe(outputParser);
 
-const message = await chatModel.invoke("what is LangSmith?");
-console.log(message.content);
+const message = await chain.invoke({
+  input: "what is LangSmith?",
+});
+
+console.log(message);
