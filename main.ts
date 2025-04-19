cb2601f75ef743e7b6040cae855983d83cbd3276
WhyK <windchimeyk@gmail.com>
2025-04-19 16:25:10 +0900

    feat: reset goals and codes

--- main.ts
+++ main.ts
@@ -1,39 +1,22 @@
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { ChatAnthropic } from "@langchain/anthropic";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
-import { DenoDOMWebBaseLoader } from "./document_loader.ts";
-import "@std/dotenv/load";
+import OpenAI from "@openai/openai";
 
-const loader = new DenoDOMWebBaseLoader(
-  "https://docs.smith.langchain.com/user_guide",
-  { selector: "main" },
-);
-const docs = await loader.load();
-const splitter = new RecursiveCharacterTextSplitter();
-const splitDocs = await splitter.splitDocuments(docs);
-
-const chatModel = new ChatAnthropic({
-  apiKey: Deno.env.get("ANTHROPIC_API_KEY"),
-});
-const prompt = ChatPromptTemplate.fromTemplate(
-  `Answer the following question based only on the provided context:
-
-<context>
-{context}
-</context>
-
-Question: {input}`,
-);
-
-const documentChain = await createStuffDocumentsChain({
-  llm: chatModel,
-  prompt,
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: Deno.env.get("OPENROUTER_API_KEY") || "",
 });
 
-const message = await documentChain.invoke({
-  input: "what is LangSmith?",
-  context: splitDocs,
-});
+async function main() {
+  const completion = await openai.chat.completions.create({
+    model: "google/gemini-2.5-pro-exp-03-25:free",
+    messages: [
+      {
+        role: "user",
+        content: "今日の天気はどう？",
+      },
+    ],
+  });
 
-console.log(message);
+  console.log(completion.choices[0].message.content);
+}
+
+main();
